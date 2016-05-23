f2cd1131fb7bba596dc87feb8456e8b34284ce95
andreyuryukin <andrey.uryukin@gmail.com>
2016-05-23 22:06:58 +0300

    commit

---
+++ app/src/main/java/com/example/andreyu/googlemap/GoogleMapsActivity.java
@@ -0,0 +1,35 @@
+package com.example.andreyu.googlemap;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    public GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng pos = new LatLng(32.321457, 34.853195);
+        mMap.addMarker(new MarkerOptions().position(pos).title("Netanya"));
+        float zoomLevel = 13;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, zoomLevel));
+    }
+}
